9ca5265534b5e1c987f567a377e6381911e47bae
Xiongyanshi <1240172230@qq.com>
2017-09-18 09:05:39 +0800

    not finished

---
+++ mortal_fibonacci_rebbits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+import sys
+
+
+def main():
+    filename == sys.argv[1]
+    with open(filename) as file:
+        file = file.read().strip().split()
+    n_month = int(file[0])
+    m_life = int(file[-1])
+
+
+if __name__ == '__main__':
+    main()
