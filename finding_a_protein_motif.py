1608bb058b2268c12da24c4bc4e7e4e17c27f514
Xiongyanshi <1240172230@qq.com>
2017-09-21 13:43:08 +0800

    use uniport module to access uniport database, but some query return nothing

--- finding_a_protein_motif.py
+++ finding_a_protein_motif.py
@@ -3,6 +3,8 @@
 
 import sys
 from Bio import SeqIO, ExPASy
+import uniprot as uni
+import StringIO
 import re
 
 
@@ -16,20 +18,35 @@ def find_motif(protein_seq):
     return loci
 
 
-def get_pro_from_uniport(id):
-    '''return protein sequence of id in uniport database'''
+def get_pro_from_SwissProt(id):
+    '''return protein sequence of id from swiss protein database
+    module used: Bio.ExPASy, SeqIO.read'''
     handle = ExPASy.get_sprot_raw(id)
-    pro_record = SeqIO.read(handle, 'swiss')
-    return str(pro_record.seq)
+    if handle:
+        pro_record = SeqIO.read(handle, 'swiss')
+        return str(pro_record.seq)
+    return None
+
+
+def get_pro_from_uniprot(id):
+    '''return protein sequence by uniprot retrieve method
+    module used:uniprot.retrieve, SeqIo.read'''
+    seq = uni.retrieve(id, 'xml')
+    if seq:
+        seq_handle = StringIO.StringIO(seq)
+        return SeqIO.read(seq_handle, 'uniprot-xml').seq
+    return None
 
 
 def main():
     with open(sys.argv[1]) as file:
         ID_list = file.read().strip().split()
     for i in ID_list:
-        print i,
+        print i
+        print 'swiss:\t', get_pro_from_SwissProt(i)
+        print 'unipr:\t', get_pro_from_uniprot(i)
+        print '\n'
         # print '{} \t- \t{}'.format(i, get_pro_from_uniport(i))
-        print find_motif(get_pro_from_uniport(i))
 
 
 if __name__ == '__main__':
