ed31ca642b8d88e98df640a7147fcdefa0cbd992
Xiongyanshi <1240172230@qq.com>
2017-09-28 22:59:05 +0800

    fix bug, ablity to find target in sequence tail

--- locating_restriction_sites.py
+++ locating_restriction_sites.py
@@ -16,8 +16,8 @@ def main():
     fasta = SeqIO.read(sys.argv[1], 'fasta')
     seq = str(fasta.seq)
 
-    for start_pos in range(0, len(seq)-4):
-        for length in range(4, 12):
+    for start_pos in range(0, len(seq)-3):
+        for length in range(4, 13):
             test_seq = seq[start_pos:start_pos+length]
             if reverse_palindrome_v2(test_seq) and len(test_seq) == length:
                 print start_pos + 1, len(test_seq), test_seq
