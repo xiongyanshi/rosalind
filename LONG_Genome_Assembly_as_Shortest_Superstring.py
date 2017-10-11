65acca305781254c7f3a6da0deeeb6b966f3969d
yanshi xiong <1240172230@qq.com>
2017-10-11 09:08:37 +0800

    update genome assembly

--- LONG_Genome_Assembly_as_Shortest_Superstring.py
+++ LONG_Genome_Assembly_as_Shortest_Superstring.py
@@ -3,6 +3,7 @@
 
 import sys
 from Bio import SeqIO
+import re
 
 
 def glueTwoSeq(seq1, seq2):
@@ -11,29 +12,36 @@ def glueTwoSeq(seq1, seq2):
     return False if glue failed
     """
     # print '\nconnecting {} AND {}'.format(seq1, seq2)
+    if seq1 in seq2:
+        print '-contain!'
+        return seq2
+    elif seq2 in seq1:
+        print '-contain!'
+        return seq1
 
-    print '\nforward connecting:',
-    print seq1[:5], '...', seq1[-5:], '--', seq2[:5], '...', seq2[-5:]
-    for loci in range(len(seq2)/2, len(seq2)):
+    wid = len(seq1) if len(seq1) < len(seq2) else len(seq2)
+    print '\nForward gluing...',
+    # print seq1[:5], '...', seq1[-5:], '--', seq2[:5], '...', seq2[-5:],
+    for loci in range(wid/2, wid+1):
         overlap_seq2 = seq2[:loci]
         if seq1[-loci:] == overlap_seq2:
-            print 'find overlap:\n{}\nlength:{}'.format(overlap_seq2,
-                                                        len(overlap_seq2))
+            print '\nFind {}bases overlap'.format(len(overlap_seq2))
             return seq1 + seq2[loci:]
-    print 'forward connect failed'
 
-    print 'reverse connecting:',
-    print seq2[:5], '...', seq2[-5:], '--', seq1[:5], '...', seq1[-5:]
-    for loci in range(len(seq1)/2, len(seq1)):
+    print 'Reverse gluing...',
+    # print seq2[:5], '...', seq2[-5:], '--', seq1[:5], '...', seq1[-5:],
+    for loci in range(wid/2, wid+1):
         overlap_seq1 = seq1[:loci]
         if seq2[-loci:] == overlap_seq1:
-            print 'find overlap:\n{}\nlength:{}'.format(overlap_seq1,
-                                                        len(overlap_seq1))
+            print '\nFind {}bases overlap'.format(len(overlap_seq1))
             return seq2 + seq1[loci:]
-    print 'reverse connect failed'
 
-    print 'cannot connect this two seqs'
     return False
+
+
+def glueSeqs(seq1, seq2):
+    """Glue two sequences into one, return False if failed"""
+    pass
 
 
 def assembly_sequences(seqs):
@@ -41,17 +49,29 @@ def assembly_sequences(seqs):
     Assembly all sequences in seqs into a complete one
     """
     while len(seqs) != 1:
-        start_seq = seqs[0]
-        seqs = seqs[1:]
-        for each in seqs:
-            glue_seq = glueTwoSeq(start_seq, each)
-            if glue_seq:
-                seqs.append(glue_seq)
+        for each in seqs[1:]:
+            contig = glueTwoSeq(seqs[0], each)
+            if contig:
+                seqs.append(contig)
+                seqs.remove(seqs[0])
                 seqs.remove(each)
-                print 'assemblized:\n{}'.format(glue_seq)
-                # print seqs
+                print 'assemblized:\n{}'.format(contig)
+                print '{} sequences remained'.format(len(seqs))
                 break
+            else:
+                pass
     return seqs[0]
+
+
+def verify(superString, sequence_collection):
+    """
+    verify if every sequence in sequence_colllection
+    is a substring of superString
+    """
+    for each in sequence_collection:
+        if each not in superString:
+            return False
+    return True
 
 
 def main():
@@ -71,12 +91,14 @@ def main():
     result_sequence_1 = assembly_sequences(sequence_list)
 
     sequence_list.reverse()
-    result_sequence_2 = assembly_sequences(sequence_list)
+    # result_sequence_2 = assembly_sequences(sequence_list)
 
     print '\nresult1: \n{}\nlength:{}'.format(result_sequence_1,
                                               len(result_sequence_1))
-    print '\nresult2: \n{}\nlength:{}'.format(result_sequence_2,
-                                              len(result_sequence_2))
+    print verify(result_sequence_1, sequence_list)
+    # print '\nresult2: \n{}\nlength:{}'.format(result_sequence_2,
+    #                                           len(result_sequence_2))
+    # print verify(result_sequence_2, sequence_list)
 
     print 'ATTAGACCTGCCGGAATAC'
 
