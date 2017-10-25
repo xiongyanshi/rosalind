7c0ad33fe6fe9399076e99212ab4b6ab3bb2ffc8
yanshi xiong <1240172230@qq.com>
2017-10-25 12:54:59 +0800

    solved, mlpy module used

--- LGIS_Longest_increasing_subsequence.py
+++ LGIS_Longest_increasing_subsequence.py
@@ -2,37 +2,41 @@
 # _*_ coding: utf-8 _*_
 
 import sys
-import itertools
+import mlpy
+# import itertools
 
 
-def subseq(sequence):
-    """return all sub-sequences as a list"""
-    length = len(sequence)
+def incre_sub(sequence):
+    """return all the increasing subsequences as a list"""
     result = []
-    for i in range(2, length):
-        delete_index = itertools.permutations(range(length), i)
-        for loci in delete_index:
-            if increase(map(int, loci)):
-                seq = [sequence[i] for i in loci]
-                result.append(seq)
+    for i in range(len(sequence)):
+        i_above = [sequence[i], ]
+        for j in sequence[i:]:
+            if j > sequence[i]:
+                i_above.append(j)
+
+        result.append(i_above)
     return result
 
 
-def increase(list, incre=True):
-    """retur True if items in list is in increasing trend"""
-    for i in range(len(list) - 1):
-        if incre:
-            if list[i + 1] < list[i]:
-                return False
-        else:
-            if list[i + 1] > list[i]:
-                return False
-    return True
+def lcs(xstr, ystr):
+    """
+    >>> lcs('thisisatest', 'testing123testing')
+    'tsitest'
+    """
+    if not xstr or not ystr:
+        return ""
+    x, xs, y, ys = xstr[0], xstr[1:], ystr[0], ystr[1:]
+    if x == y:
+        return x + lcs(xs, ys)
+    else:
+        return max(lcs(xstr, ys), lcs(xs, ystr), key=len)
 
 
 def longest_increasing_subseq(input_list, incre=True):
     """
     return the longest increasing subsequence
+    """
     """
     subseqs = subseq(input_list)
     if incre:
@@ -41,21 +45,32 @@ def longest_increasing_subseq(input_list, incre=True):
     else:
         decre_subseq = [sub for sub in subseqs if increase(sub, False)]
         return max(decre_subseq, key=len)
+    """
+    pass
 
 
 def main():
     with open(sys.argv[1]) as handle:
         file = handle.read()
+    n = int(file.split('\n')[0])
     input_list = map(int, file.split('\n')[1].split())
-    print input_list
-
-    longest_incre_sub = longest_increasing_subseq(input_list)
-    for i in longest_incre_sub:
+    print n # , input_list
+    x_incre = range(1, n + 1)
+    x_decre = x_incre[::-1]
+    y = input_list
+    incre_path = mlpy.lcs_std(x_incre, y)[1][1]
+    decre_path = mlpy.lcs_std(x_decre, y)[1][1]
+    incre_sub_list = [input_list[i] for i in incre_path]
+    decre_sub_list = [input_list[i] for i in decre_path]
+    for i in incre_sub_list:
         print i,
     print
-    longest_decre_sub = longest_increasing_subseq(input_list, False)
-    for j in longest_decre_sub:
+    for j in decre_sub_list:
         print j,
+    '''
+    for i in incre_sub(input_list):
+        print len(i), i
+    '''
 
 
 if __name__ == '__main__':
