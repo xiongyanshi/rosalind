4c6e2600555909e8757a4386380bdf909aaa08f7
ysh8 <1240172230@qq.com>
2021-07-30 22:01:35 +0800

    fix reverse complement

--- c/complementing_a_strand_of_dna.c
+++ c/complementing_a_strand_of_dna.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
+char seq[1000];
+void reverse(char *s);
+void complement(char *s);
+
+void reverse(char *s)
+{
+	char *start, *end, tmp;
+
+	start = s;
+	end = s + strlen(s) - 1;
+
+	while (start < end)
+	{
+		tmp = *start;
+		*start = *end;
+		*end = tmp;
+		start++;
+		end--;
+	}
+}
+
+void complement(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s == 'A') *s = 'T';
+		else if (*s == 'T') *s = 'A';
+		else if (*s == 'C') *s = 'G';
+		else if (*s == 'G') *s = 'C';
+		s++;
+	}
+}
+
+
 int main()
 {
-	int n;
-	char seq[1000];
+	fgets(seq, 1000, stdin);
+	reverse(seq);
+	complement(seq);
+	printf("%s\n", seq);
 
 }
