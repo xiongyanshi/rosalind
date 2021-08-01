1e9a21e6f99f4d70f70c6b0664a4a4fd3a5282e1
ysh8 <1240172230@qq.com>
2021-08-01 21:43:49 +0800

    parse fasta file, solve name

--- c/computing_gc_content.c
+++ c/computing_gc_content.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LENMAX 1024
 
-int main(int argc, char **argv)
+int main()
 {
-	FILE *input;
-	input = fopen( argv[1], "r");
-	fclose(input);
+	char name[20];
+	char seq[1000];
+
+	int in_name = 0;
+	int in_seq = 0;
+
+	char c;
+	int gc_base;
+
+	while ((c = getchar()) != EOF)
+	{
+		if ((c == '>') && (! in_name))
+			in_name = 1;
+		else if (in_name)
+		{
+			if (c == '\n')
+			{
+				printf("name: %s\n", name);
+				name[0] = '\0';
+				in_name = 0;
+				in_seq = 1;
+			}
+			else
+			{
+				strncat(name, &c, 1);
+			}
+		}
+	}
+
 }
