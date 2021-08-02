3db4fa838d953be74c3a28a1b4bf35dca41beb86
xiongyanshi <1240172230@qq.com>
2021-08-02 10:39:47 +0800

    solve compute gc content

--- c/computing_gc_content.c
+++ c/computing_gc_content.c
@@ -4,6 +4,20 @@
 
 #define LENMAX 1024
 
+float gc(char *s)
+{
+	int c=0;
+	int i=0;
+	while ((s[i] == 'A') || (s[i] == 'C') || (s[i] == 'G') || (s[i] == 'T'))
+	{
+		if (s[i] == 'G' || s[i] == 'C')
+			c++;
+		i++;
+	}
+	return  100.0*c/i;
+}
+
+
 int main()
 {
 	char name[20];
@@ -13,26 +27,61 @@ int main()
 	int in_seq = 0;
 
 	char c;
-	int gc_base;
+
+	char max_name[20];
+	float max_gc = 0.0;
+	float igc = 0.0;
 
 	while ((c = getchar()) != EOF)
 	{
-		if ((c == '>') && (! in_name))
-			in_name = 1;
+		if (c == '>')
+		{
+			if (! in_seq)
+				in_name = 1;
+			else
+			{
+				//printf("name: %s\n", name);
+				//printf("seq : %s\n", seq);
+				igc = gc(seq);
+				if (igc >= max_gc)
+				{
+					max_gc = igc;
+					strcpy(max_name, name);
+				}
+				name[0] = '\0';
+				seq[0] = '\0';
+				in_name = 1;
+				in_seq = 0;
+			}
+		}
 		else if (in_name)
 		{
 			if (c == '\n')
 			{
-				printf("name: %s\n", name);
-				name[0] = '\0';
 				in_name = 0;
 				in_seq = 1;
 			}
 			else
-			{
 				strncat(name, &c, 1);
-			}
+		}
+		else if (in_seq)
+		{
+			if (c == '\n')
+				;
+			else
+				strncat(seq, &c, 1);
 		}
 	}
+
+	//printf("name: %s\n", name);
+	//printf("seq : %s\n", seq);
+	igc = gc(seq);
+	if (igc >= max_gc)
+	{
+		max_gc = igc;
+		strcpy(max_name, name);
+	}
+
+	printf("%s\n%f\n", max_name, max_gc);
 
 }
